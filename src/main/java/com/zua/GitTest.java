9fe91e03ce0ce94462c7a83ac3382ca28a79c4d7
Arrow <1619958177@qq.com>
2022-05-07 14:10:29 +0800

    hot_fix commit 1!

--- src/main/java/com/zua/GitTest.java
+++ src/main/java/com/zua/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello World1!");
         System.out.println("Hello World2!");
         System.out.println("Hello World3!");
+        System.out.println("hot_fix World!");
     }
 }
