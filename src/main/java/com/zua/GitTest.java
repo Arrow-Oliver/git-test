e9077713e0bb443cb96a5eb40e5d897c40283de5
Arrow <1619958177@qq.com>
2022-05-07 13:47:29 +0800

    commit 1!

---
+++ src/main/java/com/zua/GitTest.java
@@ -0,0 +1,11 @@
+package com.zua;
+
+/**
+ * @author Arrow
+ * @date 2022/5/7 13:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
